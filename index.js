9bf01e1ecbcc63f2e3a4032ae4bd82e8d62ea25a
Nyash-Mauro <mauricee423@gmail.com>
2020-08-22 14:54:06 +0300

    added calculating function to the index.js

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ function getInput(){
         return false;
     }
 }
+
+function calculateDay(){
+    getInput();
+    dayOfWeek = ((((century/4)-2*century-1)+(5*year/4) + ((26*(month+1)/10))+ dayOfMonth)% 7) -1;
+    console.log(dayOfWeek);
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0){
+        dayOfWeek = dayOfWeek * -1;    }
+}
+else if (dayOfWeek > 0){
+    return dayOfWeek;
+}
